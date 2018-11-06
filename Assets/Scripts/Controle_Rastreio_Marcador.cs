7a92c4d04b68d82b569db2c3576747986bca34a8
winy <winy2000@gmail.com>
2018-11-06 14:17:40 -0200

    marcador

---
+++ Assets/Scripts/Controle_Rastreio_Marcador.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+//saber se o marcador foi detectado para habilitar a renderização
+public class Controle_Rastreio_Marcador : MonoBehaviour {
+    ITrackableEventHandler{
+
+        private TrackableBehaviour mTrackableBehaviour;
+
+    // Use this for initialization
+    void Start() {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        } else
+        {
+            OnTrackingLost();
+        }
+
+    }
+
+    private void OnTrackingFound()
+    {
+        Renderer[] rendererComponents = GetComponentInChildren<Renderer>();
+
+        Collider[] colliderComponents = GetComponentInChildren<Collider>();
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Debug.Log("Ativando Filhos");
+            this.transform.GetChild(i).gameObject.SetActive(true);
+        }
+
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = true;
+        }
+
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = true;
+        }
+        Debug.Log("Rastreamento de " + mTrackableBehaviour.TrackableName + "encontrado")
+    }
+
+    private void OnTrackingLost()
+    {
+        Renderer[] rendererComponents = GetComponentInChildren<Renderer>();
+
+        Collider[] colliderComponents = GetComponentInChildren<Collider>();
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Debug.Log("Desativando os Filhos");
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = false;
+        }
+
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = false;
+        }
+
+        Debug.Log("Rastreamento de" + mTrackableBehaviour.TrackableName + "perdido");
+    }
+}
+
+}
---
+++ Assets/Scripts/Move_Carro.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move_Carro : MonoBehaviour {
+
+    public float velocidade;
+
+    //criar ponteiros
+    private GameObject roda_1;
+    private GameObject roda_2;
+    private GameObject roda_3;
+    private GameObject roda_4;
+
+    private GameObject cam_1a_pessoa;
+    private GameObject cam_3a_pessoa;
+
+
+    // Use this for initialization
+    void Start() { 
+   
+        roda_1 = this.transform.Find("Roda_A").gameObject;
+        roda_2 = this.transform.Find("Roda_B").gameObject;
+        roda_3 = this.transform.Find("Roda_C").gameObject;
+        roda_4 = this.transform.Find("Roda_D").gameObject;
+
+        cam_1a_pessoa = this.transform.Find("Camera_1aPessoa").gameObject;
+        cam_3a_pessoa = this.transform.Find("Camera_3aPessoa").gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            this.transform.Translate(0, 0, velocidade*Time.deltaTime);
+
+            //girar as rodas rodar em torno eixo x
+            roda_1.transform.Rotate(new Vector3(velocidade, 0, 0));
+            roda_2.transform.Rotate(new Vector3(velocidade, 0, 0));
+            roda_3.transform.Rotate(new Vector3(velocidade, 0, 0));
+            roda_4.transform.Rotate(new Vector3(velocidade, 0, 0));
+        }
+        if (Input.GetKey("s"))
+        {
+            this.transform.Translate(0, 0, -velocidade * Time.deltaTime);
+
+            //girar as rodas rodar em torno eixo x
+            roda_1.transform.Rotate(new Vector3(-velocidade, 0, 0));
+            roda_2.transform.Rotate(new Vector3(-velocidade, 0, 0));
+            roda_3.transform.Rotate(new Vector3(-velocidade, 0, 0));
+            roda_4.transform.Rotate(new Vector3(-velocidade, 0, 0));
+        }
+        if (Input.GetKey("a"))
+        {
+            this.transform.Translate(-velocidade * Time.deltaTime, 0, 0);
+
+            //girar as rodas rodar em torno eixo z
+            roda_1.transform.Rotate(new Vector3(0, 0, -velocidade));
+            roda_2.transform.Rotate(new Vector3(0, 0, -velocidade));
+            roda_3.transform.Rotate(new Vector3(0, 0, -velocidade));
+            roda_4.transform.Rotate(new Vector3(0, 0, -velocidade));
+        }
+        if (Input.GetKey("d"))
+        {
+            this.transform.Translate(velocidade * Time.deltaTime, 0, 0);
+            //girar as rodas rodar em torno eixo z
+            roda_1.transform.Rotate(new Vector3(0, 0, velocidade));
+            roda_2.transform.Rotate(new Vector3(0, 0, velocidade));
+            roda_3.transform.Rotate(new Vector3(0, 0, velocidade));
+            roda_4.transform.Rotate(new Vector3(0, 0, velocidade));
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            //this.transform.Translate(0, 10, 0);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 900f);
+        }
+        if (Input.GetKey("1"))
+        {
+            cam_1a_pessoa.SetActive(true);
+            cam_1a_pessoa.SetActive(false);
+        }
+        if (Input.GetKey("3"))
+        {
+            cam_1a_pessoa.SetActive(false);
+            cam_1a_pessoa.SetActive(true);
+        }
+        if (Input.GetKey("r"))
+        {
+            this.transform.localPosition = new Vector3(0f, 0f, 0f);
+            this.transform.localRotation = new Quaternion(0f, 0f, 0f, 0f);
+            this.GetComponent<Rigidbody>().useGravity = true;
+            this.GetComponent<Rigidbody>().isKinematic = false;
+        }
+        if (Input.GetKey("p"))
+        {
+            //SceneManager.LoadScene("7_Scene"); //iria carregar a determinada cena
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);//
+        }
+    }
+}
